53e6390d6861d009ac01f3eaf2e4d7101da6f626
Bob Ippolito <bob@redivi.com>
2014-07-03 16:25:47 -0700

    refactor build script to allow both all and display

--- build.sh
+++ build.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 set -e
 
+opt_err=$(test -e ihaskell.cabal)
+opt_all=""
+opt_display=""
+for arg in "$@"; do
+  case "$arg" in
+  all)
+    opt_all=1
+    ;;
+  display)
+    opt_display=1
+    ;;
+  *)
+    opt_err=1
+    ;;
+  esac
+done
+
 # Verify that we're in the IHaskell directory.
-if [ ! -e ihaskell.cabal ]; then
+if [ "$opt_err" ]; then
   echo "Run build.sh from inside the IHaskell directory:"
   echo "  ./build.sh all      # Install IHaskell and deps (use if first install)"
   echo "  ./build.sh          # Install only IHaskell, no deps"
@@ -23,33 +40,27 @@ cd ..
 rm -rf ~/.ipython/profile_haskell
 
 # Compile dependencies.
-if [ $# -gt 0 ]; then
-  if [ $1 = "all" ]; then
-    INSTALLS="$INSTALLS ghc-parser"
-  fi
+if [ "$opt_all" ]; then
+  INSTALLS="$INSTALLS ghc-parser"
 fi
 
 # Make ihaskell itself
 INSTALLS="$INSTALLS ."
 
 # Install ihaskell-display packages.
-if [ $# -gt 0 ]; then
-  if [ $1 = "display" ]; then
-        # Install all the display libraries
-        # However, install ihaskell-diagrams separately...
-        cd ihaskell-display
-        for dir in `ls | grep -v diagrams`
-        do
-            INSTALLS="$INSTALLS ihaskell-display/$dir"
-        done
-        cd ..
-    fi
+if [ "$opt_display" ]; then
+  # Install all the display libraries
+  # However, install ihaskell-diagrams separately...
+  cd ihaskell-display
+  for dir in `ls | grep -v diagrams`; do
+    INSTALLS="$INSTALLS ihaskell-display/$dir"
+  done
+  cd ..
 fi
 
 # Clean all required directories, just in case.
 TOP=`pwd`
-for pkg in $INSTALLS
-do
+for pkg in $INSTALLS; do
     cd ./$pkg
     cabal clean
     cd $TOP
@@ -60,8 +71,6 @@ INSTALL_DIRS=`echo $INSTALLS | tr ' ' '\n' | sed 's#^#./#' | tr ' ' '\n'`
 cabal install -j $INSTALL_DIRS --force-reinstalls
 
 # Finish installing ihaskell-diagrams.
-if [ $# -gt 0 ]; then
-  if [ $1 = "display" ]; then
-      cabal install -j ihaskell-display/ihaskell-diagrams --force-reinstalls
-    fi
+if [ "$opt_display" ]; then
+  cabal install -j ihaskell-display/ihaskell-diagrams --force-reinstalls
 fi
